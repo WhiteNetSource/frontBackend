4d7324f8b36037f6f95962f2462277beb88e92e1
WNS <9oodvibe5only@gmail.com>
2023-04-14 16:52:11 +0900

    HI

---
+++ backend/routes/todo.js
@@ -0,0 +1,135 @@
+const express = require("express");
+const { PrismaClient } = require("@prisma/client");
+
+const router = express.Router();
+
+const client = new PrismaClient();
+
+//투두 생성
+router.post("/", async (req, res) => {
+  try {
+    const { todo, userId } = req.body;
+
+    if (!todo) {
+      return res.status(400).json({ ok: false, error: "Not exist todo." });
+    }
+    if (!userId) {
+      return res.status(400).json({ ok: false, error: "Not exist userId." });
+    }
+
+    const user = await client.user.findUnique({
+      where: {
+        Id: parseInt(userId),
+      },
+    });
+
+    if (!user) {
+      return res.status(400).json({ ok: false, error: "Not exist user" });
+    }
+
+    //res.send("임시");
+    const newTodo = await client.todo.create({
+      data: {
+        todo,
+        isDone: false,
+        userId: user.id, //유저 안에 아이디 써도 상관없다 디테일 중요
+      }, // 왜 써도 되냐? 이미 불러온 값은 프론트에 저장되어있기에 꺼내 쓴거 단 서버끄면 초기화
+    });
+
+    res.json({ ok: true, todo: newTodo });
+  } catch (error) {
+    console.log(error);
+  }
+});
+//투두 조회
+
+router.get("/:userId", async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    const user = await client.user.findUnique({
+      where: {
+        id: parseInt(userId),
+      },
+    });
+
+    if (!user) {
+      return res.status(400).json({ ok: false, error: "Not exist user." });
+    }
+
+    const todos = await client.todo.findMany({
+      where: {
+        userId: parseInt(userId),
+      },
+    });
+    console.log(todos);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+// 투두 완료
+router.put("/:id/done", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { userId } = req.body;
+
+    const existTodo = await client.todo.findUnique({
+      where: {
+        id: parseInt(id),
+      },
+    });
+
+    if (!existTodo) {
+      return res.status(400).json({ ok: false, error: "Not exist todo." });
+    }
+    if (existTodo.userId !== parseInt(userId)) {
+      return res.status(400).json({ ok: false, error: "U R not todo owner." });
+    }
+
+    const updatedTodo = await client.todo.update({
+      where: {
+        id: parseInt(id),
+      },
+      data: {
+        isDone: !existTodo.isDone,
+      },
+    });
+
+    res.json({ ok: true, todo: updatedTodo });
+  } catch (error) {
+    console.error(error);
+  }
+});
+// 투두 삭제
+router.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { userId } = req.body;
+
+    const existTodo = await client.todo.findUnique({
+      where: {
+        id: parseInt(id),
+      },
+    });
+
+    if (!existTodo) {
+      return res.status(400).json({ ok: false, error: "Not exist todo." });
+    }
+    if (existTodo.userId !== parseInt(userId)) {
+      return res.status(400).json({ ok: false, error: "U R not todo owner." });
+    }
+
+    const deletedTodo = await client.todo.delete({
+      where: {
+        id: parseInt(id),
+      },
+    });
+
+    res.json({ ok: true, todo: deletedTodo });
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+module.exports = router;
